7b3fad1aa534daa5dd21d7a6ba88f5aa63ea1506
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-07 13:09:50 +0530

    prime Factor

---
+++ Prime Factors/Prime Factors/Prime Factors/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Prime_Factors
+{
+    class Program
+    {
+        
+        public static void PrimeFactors(int n)
+        {
+            while(n % 2 == 0)
+            {
+                Console.WriteLine(2 + " ");
+                n /= 2;
+            }
+
+            for (int i =3; i <= Math.Sqrt(n); i+=2)
+            {
+                while(n % i == 0)
+                {
+                    Console.WriteLine(i + " ");
+                    n /= i;
+                }
+            }
+
+            if (n > 2)
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter The Number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            PrimeFactors(n);
+            
+        }
+    }
+}
