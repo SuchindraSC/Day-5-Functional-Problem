c17621232ef71eccfbcb4f5975703507e807d67f
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-07 12:59:37 +0530

    Power of 2

---
+++ Power of 2/Power of 2/Power of 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Power_of_2
+{
+    class Program
+    {
+        static double FindPowerof2(double N)
+        {
+            double Power = Math.Pow(2, N);
+            return Power;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The Value of N: ");
+            double N = Convert.ToDouble(Console.ReadLine());
+            if (N <= 0 || N > 31)
+            {
+                Console.WriteLine("You Have Entered A Wrong! Please Enter A Right Number");
+            }
+            else
+            {
+                double Powerof2 = FindPowerof2(N);
+                Console.WriteLine("\n2^" + N + " = " + Powerof2);
+            }
+        }
+    }
+}
