85bd64f81689add430805669c6ab3a3ec1048d47
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-07 12:56:06 +0530

    Leap Year

---
+++ Leap Year/Leap Year/Leap Year/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Leap_Year
+{
+    class Program
+    {
+        static void FindLeapYear()
+        {
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0)
+            {
+                if (year % 100 != 0 || year % 4 != 0)
+                {
+                    Console.WriteLine(+year + " is a Leap Year");
+                }
+            }
+            else
+            {
+                Console.WriteLine(+year + " is not a Leap Year");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The Year: ");
+            FindLeapYear();
+        }
+    }
+}
