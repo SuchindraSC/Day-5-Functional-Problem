cf2f43ff638351b4abcea1c2a092e020699718ed
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-07 13:14:52 +0530

    Quotients and Remainder

---
+++ Quotient and Remainder/Questions and Remainders/Questions and Remainders/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Questions_and_Remainders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number 1: ");
+            double number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Number 2: ");
+            double number2 = int.Parse(Console.ReadLine());
+            double quotient = number1 / number2;
+            double remainder = number1 % number2;
+            Console.WriteLine("Quotient is " + quotient);
+            Console.WriteLine("Remainder is " + remainder);
+        }
+    }
+}
