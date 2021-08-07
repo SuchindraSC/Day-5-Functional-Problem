755d5a4f00bdaaa19dd784961d2315e19d9d8dbe
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-07 13:18:15 +0530

    Swap Two Numbers

---
+++ Swap Two Numbers/Swap Two Numbers/Swap Two Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Swap_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, temp;
+            Console.WriteLine("Befor Swapping: ");
+            Console.WriteLine("\nEnter the First Number: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nEnter the Second Number: ");
+            num2 = int.Parse(Console.ReadLine());
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine("\nAfter Swapping: ");
+            Console.WriteLine("\nFirst Number: " + num1);
+            Console.WriteLine("\nSecond Number: " + num2);
+            Console.ReadLine();
+        }
+    }
+}
