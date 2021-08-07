da3d97c043e96bc9bb360a0b35e3fa4f81371ab5
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-07 13:20:44 +0530

    Number is Even or Odd

---
+++ Number is Even or Odd/Number is Even or Odd/Number is Even or Odd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Number_is_Even_or_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number;
+            Console.WriteLine("Enter The Number: ");
+            Number = int.Parse(Console.ReadLine());
+            if (Number % 2 == 0)
+            {
+                Console.WriteLine("\nThe Given Number is Even");
+            }
+            else
+            {
+                Console.WriteLine("\nThe Given Number is Odd");
+            }
+        }
+    }
+}
